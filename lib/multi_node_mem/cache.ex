069ae9ed64d26bcaee779931a6b48cf958f14851
GPad <gpadovani@gmail.com>
2017-05-26 17:10:07 +0200

    Init mnesia outside mem

--- lib/multi_node_mem/cache.ex
+++ lib/multi_node_mem/cache.ex
@@ -3,8 +3,7 @@ defmodule MultiNodeMem.Cache do
     worker_number:      2,       # (optional, default: 2) how many processes in worker pool
 
     maxmemory_size:     "1000M", # (optional, default: nil) max memory used, support such format: [1000, "10k", "1GB", "1000 K"]
+    default_ttl:        300,     # (optional, default: nil) default expire seconds for set/2
     maxmemory_strategy: :lru,    # ([:lru, :ttl, :fifo]) strategy for cleaning memory
     persistence:        true    # (optional, default: false) whether enable persistence
 end
-
-    #default_ttl:        300,     # (optional, default: nil) default expire seconds for set/2
--- lib/multi_node_mem/cache_supervisor.ex
+++ lib/multi_node_mem/cache_supervisor.ex
@@ -1,5 +1,6 @@
 defmodule MultiNodeMem.Cache.Supervisor do
   use Supervisor
+  require Logger
 
   def start_link do
     Supervisor.start_link(__MODULE__, [])
@@ -7,7 +8,23 @@ defmodule MultiNodeMem.Cache.Supervisor do
 
   def init([]) do
     nodes = [:"e1@tardis", :"e2@tardis"]
-    [ MultiNodeMem.Cache.child_spec(nodes),
+
+    Distribution.connect_to_nodes(nodes)
+    if (Distribution.first_node?(nodes, node())) do
+      Distribution.stop_mnesia(nodes)
+      Logger.debug ">>> Creating mnesia schema for: #{inspect nodes} ..."
+      ret = :mnesia.create_schema(nodes)
+      Logger.debug ">>> Creating mnesia schema: #{inspect ret} !!!"
+      Distribution.start_mnesia(nodes)
+    else
+      # Application.stop(:mnesia)
+      Logger.debug ">>> Wait mnesia... "
+      if Distribution.wait_mnesia_starting(10) == :timeout do
+        :mnesia.start()
+      end
+    end
+
+    [ MultiNodeMem.Cache.child_spec([nodes: nodes, init_mnesia: false]),
     ] |> supervise(strategy: :one_for_one)
   end
 
